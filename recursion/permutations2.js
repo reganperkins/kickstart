7ceffb26de546259e00191f604bbdf6e5872f423
reganperkins <reganlperkins@gmail.com>
2021-10-27 16:00:46 -0700

    add working but imperfect solution

--- recursion/permutations2.js
+++ recursion/permutations2.js
@@ -17,26 +17,53 @@ Constraints:
   -10 <= nums[i] <= 10
 */
 
-function helper(nums, i, slate, results) {
-  if (i == nums.length) {
+// function helper(nums, i, slate, results) {
+//   if (i === nums.length) {
+//     results.push([...slate]);
+//     return;
+//   }
+//   console.log(nums[i] === nums[i - 1], slate, nums, i);
+//   if (i > 0 && nums[i] === nums[i - 1]) return;
+
+//   for (let j = i; j < nums.length; j++) {
+//     [nums[j], nums[i]] = [nums[i], nums[j]];
+//     slate.push(nums[i]);
+//     helper(nums, i + 1, slate, results);
+//     slate.pop();
+//     [nums[i], nums[j]] = [nums[j], nums[i]];
+//   }
+// }
+
+// function permuteUnique(nums) {
+//   const results = [];
+//   const sortedNums = nums.sort((a, b) => a - b);
+//   helper(sortedNums, 0, [], results);
+//   return results;
+// }
+
+
+
+
+
+function backtracking(nums, slate, results) {
+  if (nums.length === 0) {
     results.push([...slate]);
     return;
   }
-  console.log(nums[i] === nums[i - 1], slate, nums, i);
-  if (i > 0 && nums[i] === nums[i - 1]) return;
 
-  for (let j = i; j < nums.length; j++) {
-    [nums[j], nums[i]] = [nums[i], nums[j]];
+  for (let i = 0; i < nums.length; i++) {
+    if (i !== 0 && nums[i] === nums[i - 1]) continue;
+
     slate.push(nums[i]);
-    helper(nums, i + 1, slate, results);
-    slate.pop();
-    [nums[i], nums[j]] = [nums[j], nums[i]];
+    nums.splice(i, 1);
+    backtracking(nums, slate, results);
+    nums.splice(i, 0, slate.pop());
   }
 }
 
 function permuteUnique(nums) {
+  nums.sort((a, b) => a - b);
   const results = [];
-  const sortedNums = nums.sort((a, b) => a - b);
-  helper(sortedNums, 0, [], results);
+  backtracking(nums, [], results);
   return results;
-}
+};
